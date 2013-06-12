6d80b90d72ae30cc66e18ee9d153df8d140660c4
Hidetoshi Niisaka <niisaka@live.jp>
2013-06-12 14:00:20 +0900

    予約枠を微調整

--- EpgTimer/EpgTimer/EpgView/ProgramView.xaml.cs
+++ EpgTimer/EpgTimer/EpgView/ProgramView.xaml.cs
@@ -235,8 +235,8 @@ namespace EpgTimer.EpgView
                 foreach (ReserveViewItem info in reserveList)
                 {
                     Rectangle rect = new Rectangle();
-                    rect.Opacity = 0.5;
-                    rect.Effect = new System.Windows.Media.Effects.DropShadowEffect() { BlurRadius = 10 };
+                    rect.Opacity = 0.3;
+                    rect.Effect = new System.Windows.Media.Effects.DropShadowEffect() { BlurRadius = 6 };
 
                     SolidColorBrush color;
                     if (info.ReserveInfo.RecSetting.RecMode == 5)
@@ -275,7 +275,7 @@ namespace EpgTimer.EpgView
                     rect.MouseRightButtonDown += new MouseButtonEventHandler(epgViewPanel_MouseRightButtonDown);
 
                     Canvas.SetLeft(rect, info.LeftPos);
-                    Canvas.SetTop(rect, info.TopPos - 2);
+                    Canvas.SetTop(rect, info.TopPos);
                     Canvas.SetZIndex(rect, 10);
                     canvas.Children.Add(rect);
                     reserveBorder.Add(rect);
